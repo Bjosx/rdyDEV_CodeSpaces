ee2ed71f4f891e37167597ef08160992b265b98c
Brendan O'Sullivan <50392742+Bjosx@users.noreply.github.com>
2023-03-21 02:49:31 +0000

    Task 3 Full Name Method + Testing

---
+++ spec/models/student_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Student, type: :model do
+    let(:student) { build :student_with_data }
+    describe "#full_name" do
+        it "returns the full name of a student" do
+            expect(student.full_name).to eq "#{student.title} #{student.first_name} #{student.middle_name} #{student.last_name}" 
+        end
+    end
+end
