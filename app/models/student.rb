a243583acd6ad5bfeafa889e1d211bba8b346551
Brendan O'Sullivan <50392742+Bjosx@users.noreply.github.com>
2023-03-28 03:35:02 +0000

    Tab Spacing Changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,8 @@
 #Model for student, where basic personal information of any students is stored.
 class Student < ActiveRecord::Base
-    #returns a student's full name and title as a string
-    #uses the compact method to remove any values that do not exist
-    def full_name
-        [title, first_name, middle_name, last_name].compact.join(" ")
-    end
+  #returns a student's full name and title as a string
+  #uses the compact method to remove any values that do not exist
+  def full_name
+    [title, first_name, middle_name, last_name].compact.join(" ")
+  end
 end
--- spec/factories/student.rb
+++ spec/factories/student.rb
@@ -1,15 +1,15 @@
 require 'faker'
 #factory for creating an example student. using nested factory to try and follow best practice as outlined here: https://github.com/thoughtbot/factory_bot/blob/main/GETTING_STARTED.md#best-practices
 FactoryBot.define do
-    factory :student do
-        factory :student_with_data do
-            title { Faker::Name.prefix }
-            first_name { Faker::Name.first_name }
-            middle_name { Faker::Name.middle_name }
-            last_name { Faker::Name.last_name }
-            email { Faker::Internet.email }
-            birth_date { Faker::Internet.email }
-            gender { Faker::Gender.binary_type }
-        end
+  factory :student do
+    factory :student_with_data do
+      title { Faker::Name.prefix }
+      first_name { Faker::Name.first_name }
+      middle_name { Faker::Name.middle_name }
+      last_name { Faker::Name.last_name }
+      email { Faker::Internet.email }
+      birth_date { Faker::Internet.email }
+      gender { Faker::Gender.binary_type }
     end
+  end
 end
