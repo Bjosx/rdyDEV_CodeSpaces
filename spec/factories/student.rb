6cd05ef124188f3622721ee7321f4381efedf24f
Brendan O'Sullivan <50392742+Bjosx@users.noreply.github.com>
2023-05-01 10:54:29 +0000

    remove unnecessary factory comment

--- spec/factories/student.rb
+++ spec/factories/student.rb
@@ -1,5 +1,4 @@
 require 'faker'
-#factory for creating an example student. using nested factory to try and follow best practice as outlined here: https://github.com/thoughtbot/factory_bot/blob/main/GETTING_STARTED.md#best-practices
 FactoryBot.define do
   factory :student do
     first_name { Faker::Name.first_name }
