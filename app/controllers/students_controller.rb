ea271a935ea56442d6c455189cfc1ea3e232ed1a
Brendan O'Sullivan <50392742+Bjosx@users.noreply.github.com>
2023-05-29 12:58:04 +0000

    getting started on task 6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.paginate(:page => params[:page], :per_page => 10)
+    @students = Student.paginate(page: params[:page], per_page: 10)
+  end
+  def show
+    @student = Student.find(params[:id])
+  end
+  def edit
+    @student = Student.find(params[:id])
   end
 end
