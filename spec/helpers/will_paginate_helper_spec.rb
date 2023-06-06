8b8f21a7384942b6f7bac7d646d6ddcc60487540
Brendan O'Sullivan <50392742+Bjosx@users.noreply.github.com>
2023-06-06 12:43:07 +0000

    changed will_paginate_helper tests to better reflect the changes I made to the class, rather than testing gem behaviour

--- spec/helpers/will_paginate_helper_spec.rb
+++ spec/helpers/will_paginate_helper_spec.rb
@@ -11,24 +11,20 @@ RSpec.describe WillPaginateHelper do
         expect(helper.will_paginate_nicely(students)).to match /<ul class=\"pagination\">/
       end
 
-      it "includes five page links" do
-        expect(helper.will_paginate_nicely(students).scan("href='/students?page=").size).to eq(5)
+      it "includes links to /students pages" do
+        expect(helper.will_paginate_nicely(students).scan("href='/students?page=").size).to be >= 1
       end
 
-      it "sets the < button as disabled" do
-        expect(helper.will_paginate_nicely(students)).to match Regexp.escape("<li class=\"disabled\"><a><</a></li>")
-      end
-
-      it "sets page one as active" do
+      it "gives the link to the current page a class of active" do
         expect(helper.will_paginate_nicely(students)).to match Regexp.escape("<li class=\"active\"><a href='/students?page=1'>1</a>")
+      end
+
+      it "does not set any other pages as active" do
+        expect(helper.will_paginate_nicely(students).scan("<li class=\"active\">").size).to be eq(1)
       end
 
       it "includes a divider (...)" do
         expect(helper.will_paginate_nicely(students)).to match Regexp.escape("...</a></li>")
-      end
-
-      it "makes > a link to page 2" do
-        expect(helper.will_paginate_nicely(students)).to match Regexp.escape("<li><a href='/students?page=2'>></a></li>")
       end
     end
 
@@ -36,32 +32,12 @@ RSpec.describe WillPaginateHelper do
       let!(:create_students) { create_list(:student, 60, :full_data) }
       let(:students) { Student.paginate(page: 3, per_page: 10) }
 
-      it "includes eight page links" do
-        expect(helper.will_paginate_nicely(students).scan("href='/students?page=").size).to eq(8)
-      end
-
-      it "makes > a link to page 4" do
-        expect(helper.will_paginate_nicely(students)).to match Regexp.escape("<li><a href='/students?page=4'>></a></li>")
-      end
-
-      it "makes < a link to page 2" do
-        expect(helper.will_paginate_nicely(students)).to match Regexp.escape("<li><a href='/students?page=2'><</a></li>")
-      end
-
-      it "sets page three as active" do
+      it "sets the current page (three) as active" do
         expect(helper.will_paginate_nicely(students)).to match Regexp.escape("<li class=\"active\"><a href='/students?page=3'>3</a>")
       end
 
-      it "doesn't includes a divider (...)" do
-        expect(helper.will_paginate_nicely(students)).not_to match Regexp.escape("...</a></li>")
-      end
-    end
-    
-    context "There is less than a page worth of records" do
-      let!(:create_students) { create_list(:student, 7, :full_data) }
-      let(:students) { Student.paginate(page: 1, per_page: 10) }
-      it "returns nothing" do
-        expect(helper.will_paginate_nicely(students)).to be_nil
+      it "does not set any other pages as active" do
+        expect(helper.will_paginate_nicely(students).scan("<li class=\"active\">").size).to eq(1)
       end
     end
   end
