9ae1125bda0ec6ca04d8a9c7c7a8707e604a2ab7
Matthew White <matthew@realemail.net>
2023-06-14 05:02:55 +0000

    TASK-4: Restore history after branch squash

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe Student, type: :model do
   let(:student2) { build :student, first_name: "Brendan", last_name: "O'Sullivan" }
 
   it { should validate_presence_of(:first_name) }
+
   it { should validate_presence_of(:last_name) }
 
   describe "#full_name" do
     it "returns the full name of a student" do
       expect(student.full_name).to eq "#{student.title} #{student.first_name} #{student.middle_name} #{student.last_name}" 
     end
+    
     it "returns only fields which are not nil" do
       expect(student2.full_name).to eq "Brendan O'Sullivan" 
     end
