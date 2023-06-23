20e2d0326e9d3c9484419373c40f0e98b1c49d43
Brendan O'Sullivan <50392742+Bjosx@users.noreply.github.com>
2023-06-23 03:04:17 +0000

    Removed extra blank space, moved styling into external sheet and moved default per_page into a constant

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
 class StudentsController < ApplicationController
+  PER_PAGE = 10
   def index
-    @students = Student.paginate(:page => params[:page], :per_page => 10)
+    @students = Student.paginate(page: params[:page], per_page: PER_PAGE)
   end
 end
