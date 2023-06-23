1afb4e1d850c20b12b3530c13afe0961fd06614e
Brendan O'Sullivan <50392742+Bjosx@users.noreply.github.com>
2023-06-23 04:59:06 +0000

    updated css to American English for consistency. Moved renderer out of will_paginate arguments and into the module as its own defined class

--- app/helpers/will_paginate_helper.rb
+++ app/helpers/will_paginate_helper.rb
@@ -1,4 +1,32 @@
 module WillPaginateHelper
+  class CustomRenderer < WillPaginate::ActionView::LinkRenderer
+    def to_html
+      html = '<ul class="pagination">'
+      html += pagination.map do |item|
+        item.is_a?(Integer) ?
+          page_number(item) :
+          send(item)
+      end.join()
+      html += '</ul>'
+    end
+    def page_number(page)
+      if page == current_page
+        tag(:li, "<a href='/students?page=#{page}'>#{page}</a>", class: "active")
+      else
+        tag(:li, "<a href='/students?page=#{page}'>#{page}</a>")
+      end
+    end
+    def previous_or_next_page(page, text, classname, aria_label = nil)
+      if page
+        tag(:li, "<a href='/students?page=#{page}'>#{text}</a>")
+      else
+        tag(:li, "<a>#{text}</a>", class: "disabled")
+      end
+    end
+    def gap
+      tag(:li, "<a href=''>...</a>", class: "disabled")
+    end
+  end
   def will_paginate_nicely(collection)
     will_paginate(
       collection, 
@@ -6,34 +34,7 @@ module WillPaginateHelper
       outer_window: 0,
       next_label: '>',
       previous_label: '<',
-      renderer: Class.new(WillPaginate::ActionView::LinkRenderer) do
-        def to_html
-          html = '<ul class="pagination">'
-          html += pagination.map do |item|
-            item.is_a?(Integer) ?
-              page_number(item) :
-              send(item)
-          end.join()
-          html += '</ul>'
-        end
-        def page_number(page)
-          if page == current_page
-            tag(:li, "<a href='/students?page=#{page}'>#{page}</a>", class: "active")
-          else
-            tag(:li, "<a href='/students?page=#{page}'>#{page}</a>")
-          end
-        end
-        def previous_or_next_page(page, text, classname)
-          if page
-            tag(:li, "<a href='/students?page=#{page}'>#{text}</a>")
-          else
-            tag(:li, "<a>#{text}</a>", class: "disabled")
-          end
-        end
-        def gap
-          tag(:li, "<a href=''>...</a>", class: "disabled")
-        end
-      end
+      renderer: CustomRenderer
     )
   end
 end
